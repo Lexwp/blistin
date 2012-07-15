c9507c8fb48c90c3a876218fb96966bd52ca8829
Lex Beelen <lex@lexperts.nl>
2012-07-15 22:33:38 +0200

    setup main

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -1,27 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Main extends CI_Controller {
+class Main extends MY_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('main');
+	public function index(){
+			
+		$data['title']	= 'Blistin';
+		$data['meta'] = array(
+       		array('name' => 'robots', 'content' => 'no-cache'),
+        	array('name' => 'description', 'content' => ''),
+        	array('name' => 'Content-type', 'content' => 'text/html; charset=utf-8', 'type' => 'equiv')
+    	);
+				
+		$data['view'] 	= 'main';
+		$this->load->view('template', $data);
+		
 	}
+	
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
